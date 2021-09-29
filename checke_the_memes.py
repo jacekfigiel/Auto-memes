26bb34c266eec253c3e3d1d38a8ea053d280002b
jacekfigiel <jacekfigo1988@gmail.com>
2021-09-29 06:30:22 +0100

    Second commit different website

--- checke_the_memes.py
+++ checke_the_memes.py
@@ -3,12 +3,11 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 browser = webdriver.Chrome()
-browser.get("https://jbzd.com.pl/")
+browser.get("https://memes.com/")
 html_elm = browser.find_element_by_tag_name("html")
 time.sleep(5)
-browser.find_element_by_class_name("MuiTouchRipple-root").click()
+browser.fullscreen_window()
 time.sleep(5)
-for i in range(5):
-    html_elm.send_keys(Keys.PAGE_DOWN)
-    time.sleep(10)
-html_elm.send_keys(Keys.HOME)
+for i in range(900):
+    html_elm.send_keys(Keys.ARROW_DOWN)
+    time.sleep(0.4)
