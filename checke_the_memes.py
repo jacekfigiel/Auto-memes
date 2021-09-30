346669aea2caa68a33f0cdf8297eb84b1b237776
jacekfigiel <jacekfigo1988@gmail.com>
2021-09-30 06:27:56 +0100

    Press q to quit added and loop changed

--- checke_the_memes.py
+++ checke_the_memes.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import keyboard
 
 browser = webdriver.Chrome()
 browser.get("https://memes.com/")
@@ -8,6 +9,11 @@ html_elm = browser.find_element_by_tag_name("html")
 time.sleep(5)
 browser.fullscreen_window()
 time.sleep(5)
-for i in range(900):
-    html_elm.send_keys(Keys.ARROW_DOWN)
-    time.sleep(0.4)
+while True:
+    if keyboard.is_pressed("q"):
+        browser.close()
+    else:
+        html_elm.send_keys(Keys.ARROW_DOWN)
+        time.sleep(0.4)
+
+
