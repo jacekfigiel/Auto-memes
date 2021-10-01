7e1b62d5dcfe0ba41181adc5801be099df9e913f
jacekfigiel <jacekfigo1988@gmail.com>
2021-10-01 05:59:39 +0100

    Press 0 to stop for 1 sec added

--- checke_the_memes.py
+++ checke_the_memes.py
@@ -6,12 +6,13 @@ import keyboard
 browser = webdriver.Chrome()
 browser.get("https://memes.com/")
 html_elm = browser.find_element_by_tag_name("html")
-time.sleep(5)
 browser.fullscreen_window()
 time.sleep(5)
 while True:
     if keyboard.is_pressed("q"):
         browser.close()
+    elif keyboard.is_pressed("0"):
+        time.sleep(1)
     else:
         html_elm.send_keys(Keys.ARROW_DOWN)
         time.sleep(0.4)
